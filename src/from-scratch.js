8e8ea1847db9ba12a1ce3aada781b1b24e10b605
Marcy <marcy@Marcys-MacBook-Pro.local>
2024-04-04 19:35:57 -0400

    solved

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,14 +1,109 @@
-const measureRain = () => {
+const measureRain = (inches) => {
+if (inches===0) { 
+console.log('drought')
+} 
+else if (inches < 2) {
+  console.log('dry')
+} 
+else if (inches < 4) {
+  console.log('average')
+}
+else if (inches < 6) {
+  console.log('rainy')
+}
+else {
+  console.log('flood')
+}
+};
+// measureRain(3)
+
+
+
+
+
+const happyBirthdayPet = (breed,age) => {
+  if (breed==='snake') {
+    console.log('Hiss Hiss')
+  } else if (breed==='cat' && age < 5 ) {
+    console.log('Mew mew')
+  } else if (breed==='cat' && age >= 5) {
+    console.log('Meow meow')
+  } else if (breed==='dog' && age > 5 ) {
+console.log('Arf arf')
+  } else if (breed==='dog' && age >= 5 ) {
+  console.log('Woof woof')
+  } else if (breed==='dog' && age <=10 ) {
+  console.log('Boof boof')
+  }
+}
+
+// if (breed = 'snake') {
+//   console
+// } else if (breed = 'cat') {
+//   if (age < 5) {
+//     console
+//   } else if (age > 5) {
+//     console
+//   }
+// } else if (breed = 'dog')
+
+
+const funTypes = (jsType) => {
+  if (typeof jsType === 'string') {
+  console.log('That\'s just some text.') 
+
+} else if (typeof jsType === 'boolean') {
+  console.log('To bool, or not to bool?') 
+  
+} else if (typeof jsType === 'undefined') {
+  console.log('Nothing, but I didn\'t set that') 
+  
+} else if (jsType === null) {
+  console.log('Nothing, and I did see that') 
+  
+} else if (Array.isArray(jsType)) {
+  console.log('I order you to be indexed.') 
+  
+} else if (Number.isNaN(jsType)) {
+  console.log('Well, now you\'re just showing off.') 
+  
+} else if (typeof jsType === 'object') {
+  console.log('Anybody got the key?') 
+  
+} else if (typeof jsType === 'number') {
+  console.log('That\'s a good number.') 
+
+} 
 };
 
-const happyBirthdayPet = () => {
+// console.log(typeof NaN)
+
+funTypes('yo');
+funTypes(6);
+funTypes(false)
+funTypes(undefined)
+funTypes(null)
+funTypes({})
+ funTypes([])
+funTypes(NaN)
+
+
+// when they ask to return message ---> return "___"
+// to call function cosole.log(function name(____))
+
+
+const rounder = (float,roundingSetting) => {
+  if (roundingSetting==='up') { 
+      return Math.ceil(float)
+  }  else if (roundingSetting==='down')  { 
+      return Math.floor(float)
+    } else if (roundingSetting==='honest') { 
+      return Math.round(float)}
 };
 
-const funTypes = () => {
-};
-
-const rounder = () => {
-};
+console.log(rounder( 3.5, 'up'))
+console.log(rounder( 3.5, 'down'))
+console.log(rounder( 7.7, 'honest'))
 
 const fizzBuzzish = () => {
 };
--- src/modify.js
+++ src/modify.js
@@ -1,13 +1,15 @@
 const wildlyBiasedReview = (location) => {
   if (location === 'NYC') {
-    console.log('THE GREATEST CITY IN THE WORLD');
-    console.log('THE CITY THAT NEVER SLEEPS');
-    console.log('WOW WHAT A CITY');
+     console.log('THE GREATEST CITY IN THE WORLD');
+     console.log('THE CITY THAT NEVER SLEEPS');
+     console.log('WOW WHAT A CITY');
   } else {
     console.log('Yea that place is cool I guess');
-  }
+  }  
+ return "where you tryna go"
 };
 
 module.exports = {
   wildlyBiasedReview,
 };
+
